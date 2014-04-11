8da1fb293435d441870f8cf37e8829616c91d97a
esagardia <esagardia@esagardia-HP-Pavilion-dv6-Notebook-PC.(none)>
2014-04-11 16:14:57 +0200

    [usersThreads] fin de thread create

--- src/core/thread.c
+++ src/core/thread.c
@@ -3,6 +3,8 @@
 #include <ucontext.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <valgrind/valgrind.h>
+
 struct thread_list
 {
   struct list_head children;
@@ -13,6 +15,9 @@ struct thread_container;
 struct thread{
   ucontext_t uc;
   void * retval;
+#ifndef NDEBUG
+  int valgrind_stackid;
+#endif
 };
 
 struct thread_container
@@ -22,44 +27,82 @@ struct thread_container
 };
 
 static struct thread_list readylist;
+static struct thread_list waitinglist;
 
-static struct thread_container running;//TODO modifs
+static struct thread_container *running;
 
 static void thread_init() __attribute__ ((constructor));
 static void thread_init()
 {
   list_head_init(&readylist.children);
   readylist.num_children = 0;
+  running.thread = (struct thread *)malloc(sizeof(struct thread));
 }
 
 void thread_quit() __attribute__ ((destructor));
 void thread_quit(){
-  return;
+  if(running.thread)
+    free(running.thread);
+  
+  // question a faverge pour libérer le thread main et les autres  
+  
 }
 
 
 extern thread_t thread_self(void){
-  //running.thread = malloc(sizeof(struct thread)); // juste pour verifier qu'il renvoie bien le pointeur alloue
   return running.thread;
 }
 
 
 extern int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
-  //  return makecontext(newthread,func,funcarg); 
+
+  //creation de contexte 
+  ucontext_t uc;
+  uc.uc_stack.ss_size = 64*1024;
+  uc.uc_stack.ss_sp = malloc(uc.uc_stack.ss_size);
+  if(!uc.uc_stack.ss_sp){
+    perror("malloc stack ds thread_create");
+    return 1;
+  }
+  uc.uc_link = NULL;
+  makecontext(&uc,func,funcarg);
+  
+  //allocation newthread
+  newthread = (struct thread *)malloc(sizeof(struct thread));
+  if(!newthread){
+    free(uc.uc_stack.ss_sp);
+    perror("malloc newthread ds thread_create");
+    return 1;
+  }
+#ifndef NDEBUG
+  newthread.valgrind_stackid = VALGRIND_STACK_REGISTER(uc.uc_stack.ss_sp,uc.uc_stack.ss_sp + uc.uc_stack.ss_size);
+#endif
+  
+  // initialisation newthread
+  newthread->uc = uc;
+  newthread->retval = NULL;
+  struct thread_container *c = (struct thread_container)malloc(sizeof(struct thread_container));
+  c.thread = newthread;
+  list_add_tail(&readylist.children, &c.list);
+  
+  // appel de yield
+  if (thread_yield()){
+    perror("erreur appel yield ds thread_create");
+    return 1;
+  }
   return 0;
 }
 
 
+
 extern int thread_yield(void){
-  //placer le thread courant dans la liste des threads ready
-  if (!list_empty(&readylist.children)){
-    struct thread * top=list_pop_(&readylist.children,0);
-    list_add_tail(&readylist.children, &running.list);
-    //running.list = NULL;
-    running.thread = top;
-    
-    
-  }
+  //place le thread courant dans la liste des threads ready
+  struct thread_container c;
+  c.thread = running.thread;
+  list_add_tail(&readylist.children, &running.list);
+  struct thread * top=list_pop_(&readylist.children,0);
+  swapcontext(&running.thread->uc, &top.thread->uc);
+  running.thread = top;
   return 0;
 }
 
