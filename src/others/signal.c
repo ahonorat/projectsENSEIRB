fd345a763a2668e6433fb31bad8215ce5c9a08ab
thibaud b <bourdeau.thibaud@gmail.com>
2014-05-02 09:47:38 +0200

    [usersThreads] ajout include signal.h ...

--- src/others/signal.c
+++ src/others/signal.c
@@ -1,6 +1,14 @@
+#include "signal.h"
+
+void add_signal(unsigned int * listsig, int sig){
+	int mask = 1;
+	listsig = listsig | (mask << sig);
+}
+
 int thread_kill(thread_t thread,int sig){
-  thread->signal = sig;
-  return 1;
+	if(priority_level(thread->signal) <= sig)
+		thread->signal = sig;
+		return 1;
 }
 
 int thread_sigaction(int signum,void (*new_sa_handler)(int),void (*old_sa_handler)(int));
