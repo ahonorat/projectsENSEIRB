3722f72f95d2ad397d56e6482e18ea1c8a1c89a8
ahonorat <alexandre.honorat@yahoo.fr>
2014-12-22 14:44:46 +0100

    [3-5-6TDP] Suprression fichiers td4

--- 5tdp/src/comm.h
+++
@@ -1,6 +0,0 @@
-#ifndef __COMM_H__
-#define __COMM_H__
-
-void* comm(void* arguments);
-
-#define __COMM_H__
--- 5tdp/src/definitions.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DEFINITIONS_H__
-#define __DEFINITIONS_H__
-
-enum tag {ASK, END, TILE};
-
-#endif
