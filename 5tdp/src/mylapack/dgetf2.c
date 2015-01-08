11b3704e7ca7bfca72bae11fa8842e2590ac078e
KERYELL-EVEN Elouan <ekeryelleven@mitterand.pedago.ipb.fr>
2015-01-08 22:35:27 +0100

    [3-5-6TDP] 1st version of dgetf2

--- 5tdp/src/mylapack/dgetf2.c
+++ 5tdp/src/mylapack/dgetf2.c
@@ -1,5 +1,12 @@
 #include "mylapack.h"
 
+//TODO : gérer le cas où l'élément diagonal est nul en échangeant la ligne avec une ligne où l'élément de colonne k n'est pas nul
+//TODO : gérer le cas où M!=N
+//Column major version
 void mylapack_dgetf2(int * m, int * n, double * a, int * lda, int * ipiv, int * info){
-  //TODO
+  int k,j;
+  for(k=0; k < N-1; ++k){
+    myblas_dscal(N-k, 1/a[j + j*lda], a+(j+1)+j*lda, 1);
+    myblas_dger(CblasColMajor, *m-j , *n-j, -1, a+(j+1)+(j*lda), 1, a+j+(j+1)*lda, lda, a+(j+1)+(j+1)*lda, lda);
+  }
 }
