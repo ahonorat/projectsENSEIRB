cddafdbbb0924bc9a147c95ce85d3bfc746f6bd4
ahonorat <alexandre.honorat@yahoo.fr>
2015-01-09 19:22:36 +0100

    [3-5-6TDP] dgetf2 pour rectangle

--- 5tdp/src/mylapack/dgetf2.c
+++ 5tdp/src/mylapack/dgetf2.c
@@ -6,7 +6,7 @@
 #include "../util/util.h"
 
 //TODO : gérer le cas où l'élément diagonal est nul en échangeant la ligne avec une ligne où l'élément de colonne k n'est pas nul
-//TODO : gérer le cas où M!=N
+//TODO : tester le cas où M!=N, normalement c'est bon
 //Column major version
 lapack_int mylapack_dgetf2(int matrix_layout, lapack_int m, lapack_int n, double *a, lapack_int lda, lapack_int *ipiv){
   assert(matrix_layout == LAPACK_COL_MAJOR);
@@ -15,7 +15,7 @@ lapack_int mylapack_dgetf2(int matrix_layout, lapack_int m, lapack_int n, double
   for (k = 0; k < m ; ++k)
     ipiv[k] = k;
   for(k=0; k < (min_dim - 1); ++k){
-    myblas_dscal(min_dim-1-k, 1.0 / a[k+k*lda], a+k+(k+1)*lda, lda);
+    myblas_dscal(n-1-k, 1.0 / a[k+k*lda], a+k+(k+1)*lda, lda);
     myblas_dger(CblasColMajor, m-1-k , n-1-k, -1.0, a+(k+1)+k*lda, 1, a+k+(k+1)*lda, lda, a+(k+1)+(k+1)*lda, lda);
   }
   return 0;
