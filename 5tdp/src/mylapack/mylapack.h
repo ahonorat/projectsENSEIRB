a7bb6298b224cf3066f223ab373c3e3509876cb1
Elouan <elouan>
2015-01-14 05:04:16 +0100

    [3-5-6TDP] plop

--- 5tdp/src/mylapack/mylapack.h
+++ 5tdp/src/mylapack/mylapack.h
@@ -7,9 +7,14 @@
 #include <lapacke.h>
 #endif
 
-lapack_int mylapack_dgetf2(int matrix_layout, int m, int n, double *a, int lda, int *ipiv);
+//#define min(x,a,b) do {x = a<b ? a : b;}  while (0)
 
-lapack_int mylapack_dgetrf(int matrix_layout, int m, int n, double *a, int lda, int *ipiv, int *info);
+lapack_int mylapack_dgetf2(int matrix_order, lapack_int m, lapack_int n, double* a, lapack_int lda, lapack_int* ipiv);
+
+//blocked subfunction for dgetrf
+lapack_int b_mylapack_dgetrf(int matrix_order, lapack_int m, lapack_int n, double* a, lapack_int lda, lapack_int* ipiv, int bsize);
+
+lapack_int mylapack_dgetrf(int matrix_order, lapack_int m, lapack_int n, double* a, lapack_int lda, lapack_int* ipiv);
 
 lapack_int mylapack_dgesv(int matrix_layout, int n, int nrhs, double *a, int lda, int *ipiv, double *b, int ldb, int *info);
 
