a9dcaaf8ed84defe6f7284d50e99ca5ed6d8c1db
Elouan <elouan>
2015-01-19 00:42:11 +0100

    [3-5-6TDP] Fixed a bug in b_dgetrf() occuring when dimensions weren't dividable by block size. Cleaned the code of b_dgetrf(), cleaned the tests and written a new test for b_dgetrf()

--- 5tdp/src/mylapack/dgetrf.c
+++ 5tdp/src/mylapack/dgetrf.c
@@ -4,36 +4,20 @@
 #include "mylapack.h"
 #include "../myblas/myblas.h"
 
-//DEBUG
-///*
-#include <stdio.h>
-#include "../util/util.h"
-//*/
-//DEBUG
-
+#define BLOCK_SIZE 10
 
 #define MIN(a,b) (a<b)?a:b
 
 //ELOUAN : Version sans pivotage pour l'instant
 lapack_int b_mylapack_dgetrf(int matrix_order, lapack_int m, lapack_int n, double* a, lapack_int lda, lapack_int* ipiv, int bsize){
-  assert(matrix_order == CblasColMajor);
+  assert(matrix_order == LAPACK_COL_MAJOR);
   //nb_block_m : number of block by column
   //nb_block_n : number of block by row
-  //current_bsize_m : number of rows of the current processed bloc
-  //current_bsize_n : number of columns of the current processed bloc
+  //current_bsize_m : number of rows of the currently processed bloc
+  //current_bsize_n : number of columns of the currently processed bloc
   int nb_block_m,nb_block_n,min_nb_block,current_bsize_m,current_bsize_n,k,i,j;
   nb_block_m = m%bsize == 0 ? m/bsize : (m/bsize)+1;
-  //DEBUG
-  ///*
-  fprintf(stderr,"\nnb_block_m : %d\n",nb_block_m);
-  //*/
-  //DEBUG
   nb_block_n = n%bsize == 0 ? n/bsize : (n/bsize)+1;
-  //DEBUG
-  ///*
-  fprintf(stderr,"\nnb_block_n : %d\n",nb_block_n);
-  //*/
-  //DEBUG
   min_nb_block = MIN(nb_block_m, nb_block_n);
   for(k=0; k<min_nb_block; ++k){
     //Normally the diagonal block is always of size bsize*bsize, except in the last iteration of the very particular case where both dimensions of the original matrix aren't dividable by bsize
@@ -41,69 +25,34 @@ lapack_int b_mylapack_dgetrf(int matrix_order, lapack_int m, lapack_int n, doubl
     current_bsize_n = MIN(n-(k*bsize),bsize);
     //LU(A(k,k))
     mylapack_dgetf2(matrix_order, current_bsize_m, current_bsize_n, &a[k*bsize + k*bsize*lda], lda, &ipiv[k*bsize]);
-    for(i=k*bsize; i< ((k*bsize)+current_bsize_m); ++i)
-      ipiv[i] = i;
-    //DEBUG
-    ///*
-    fprintf(stderr,"\nK = %d\n\n",k);
-    fprintf(stderr,"LU A(%d,%d)\n",k,k);
-    affiche(m,n,a,lda,stderr);
-    //*/
-    //DEBUG
     current_bsize_n = bsize;
     for(i=k+1; i<nb_block_m; ++i){
       current_bsize_m = MIN(m-(i*bsize),bsize);
       //DTRSM : Solve L(i,k)U(k,k)=A(i,k)
       myblas_dtrsm(matrix_order, CblasRight, CblasUpper, CblasNoTrans, CblasUnit, current_bsize_m, current_bsize_n, 1.0, &a[k*bsize + k*bsize*lda], lda, &a[i*bsize + k*bsize*lda], lda);
-      //DEBUG
-      ///*
-      fprintf(stderr,"\n DTRSM : L(%d,%d)U(%d,%d)=A(%d,%d)\n",i,k,k,k,i,k);
-      affiche(m,n,a,lda,stderr);
-      //*/
-      //DEBUG
     }
     current_bsize_m = bsize;
     for(j=k+1; j<nb_block_n; ++j){
       current_bsize_n = MIN(n-(j*bsize),bsize);
       //DTRSM : Solve L(k,k)U(k,j)=A(k,j)
 	myblas_dtrsm(matrix_order, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit, current_bsize_m, current_bsize_n, 1.0, &a[k*bsize + k*bsize*lda], lda, &a[k*bsize + j*bsize*lda], lda);
-      //DEBUG
-      ///*
-      fprintf(stderr,"\nDTRSM : L(%d,%d)U(%d,%d)=A(%d,%d)\n",k,k,k,j,k,j);
-      affiche(m,n,a,lda,stderr);
-      //*/
-      //DEBUG
     }
     for(j=k+1; j<nb_block_n; ++j){
-      current_bsize_m = MIN(m-(j*bsize),bsize);
+      current_bsize_n = MIN(n-(j*bsize),bsize);
       for(i=k+1;i<nb_block_m; ++i){
-	current_bsize_n = MIN(n-(i*bsize),bsize);
-	//DEBUG
-	/*
-	if(k == 0 && j == 2){
-	  fprintf(stderr,"current_bsize_m = %d\n",current_bsize_m);
-	  fprintf(stderr,"current_bsize_n = %d\n",current_bsize_n);
-	}
-	*/
-	//DEBUG
-
-	//A(i,j) = A(i,j) - L(i,k)*U(k,j)
+	current_bsize_m = MIN(m-(i*bsize),bsize);
+	//DGEMM : A(i,j) = A(i,j) - L(i,k)*U(k,j)
 	myblas_dgemm_jik(matrix_order, CblasNoTrans, CblasNoTrans, current_bsize_m, current_bsize_n, bsize, -1.0, &a[i*bsize + k*bsize*lda], lda, &a[k*bsize + j*bsize*lda], lda, 1.0, &a[i*bsize + j*bsize*lda], lda);
-      //DEBUG
-      ///*
-      fprintf(stderr,"\nDGEMM : A(%d,%d) -= L(%d,%d)U(%d,%d)\n",i,j,i,k,k,j);
-      affiche(m,n,a,lda,stderr);
-      //*/
-      //DEBUG
       }
     }
   }
+  for(i=0; i<m; ++i)
+      ipiv[i] = i;
   return EXIT_SUCCESS;
 }
 
 lapack_int mylapack_dgetrf(int matrix_order, lapack_int m, lapack_int n, double* a, lapack_int lda, lapack_int* ipiv){
-  //TODO
-  return EXIT_SUCCESS;
+  return b_mylapack_dgetrf(LAPACK_COL_MAJOR, m, n, a, lda, ipiv, BLOCK_SIZE);
 }
 
 lapack_int p_mylapack_dgetrf(int matrix_layout, lapack_int m, lapack_int n, double *a, lapack_int lda, lapack_int *ipiv, lapack_int *info){
--- 5tdp/src/test/test-mylapack.c
+++ 5tdp/src/test/test-mylapack.c
@@ -16,82 +16,73 @@
 
 #define ABS(a) ((a)>0?(a):-(a))
 
+#define MIN(a,b) (a<b)?a:b
+
 //assert Erreur relative < 0.000001
 #define ASSERT_EQ(a,b) if(ABS(((a)-(b))/(a))>0.000001){printf(#a "!=" #b "(%e!=%e) (diff rel = %e)\n",a,b,ABS(((a)-(b))/(a))); assert(0);}
 
-#define MAT_SIZE_M 6
-#define MAT_SIZE_N 6
+#define MAT_SIZE_M 40
+#define MAT_SIZE_N 50
 #define MAT_SIZE_K 10
-#define BLOCK_SIZE 3
+#define BLOCK_SIZE 30
 
 void test_dget_(){
-
+  
   int i, min = (MAT_SIZE_M < MAT_SIZE_N)?MAT_SIZE_M:MAT_SIZE_N;
   int ipiv[MAT_SIZE_M]; // i.e. min (m,n)
-
+  
   double *A = matrix_rand(MAT_SIZE_M , MAT_SIZE_N);
   double* L = matrix_alloc(MAT_SIZE_M, min);
   double* U = matrix_alloc(min, MAT_SIZE_N);
   double* C = matrix_alloc(MAT_SIZE_M, MAT_SIZE_N);
   memcpy(C, A, sizeof(double)*MAT_SIZE_M*MAT_SIZE_N);
 
-  printf("Testing dget_:\n");
-
-  printf("\t mylapack_dgetf2...\n");
-  //mylapack_dgetf2(LAPACK_COL_MAJOR, MAT_SIZE_M, MAT_SIZE_N, A, MAT_SIZE_M, ipiv);
-  b_mylapack_dgetrf(LAPACK_COL_MAJOR, MAT_SIZE_M, MAT_SIZE_N, A, MAT_SIZE_M, ipiv, BLOCK_SIZE);
+  printf("Testing mylapack_dgetf2...");
+  mylapack_dgetf2(LAPACK_COL_MAJOR, MAT_SIZE_M, MAT_SIZE_N, A, MAT_SIZE_M, ipiv);
   matrix_AtoLU(MAT_SIZE_M, MAT_SIZE_N, A, MAT_SIZE_M, L, MAT_SIZE_M, U, min);
   cblas_dgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,
 	      MAT_SIZE_M,MAT_SIZE_N,min,
 	      1.0,L,MAT_SIZE_M,
-	          U,min,
+	      U,min,
               0.0,A,MAT_SIZE_M);
   for(i=0;i<MAT_SIZE_M*MAT_SIZE_N;i++)
     ASSERT_EQ(A[i],C[i]);
-  printf("ok\n");
-
-  /*
-  printf("\t mylapack_dgetrf...\n");
-  int m = 4;
-  int n = 5;
-  int bsize = 2;
-  int min_mn = (m < n)?m:n;
-  lapack_int* ipiv2;
-  ipiv2 = malloc(m*sizeof(lapack_int));
-  double *A2 = matrix_alloc(m , n);
-  double *L2 = matrix_alloc(m, min_mn);
-  double *U2 = matrix_alloc(min_mn, n);
-  A2[0] = 1;  A2[4] = 2;   A2[8] = 2;  A2[12] = 2;  A2[16] = 2;
-  A2[1] = 2;  A2[5] = 7;  A2[9] = 10; A2[13] = 10; A2[17] = 10;
-  A2[2] = 4; A2[6] = 13; A2[10] = 14; A2[14] = 30; A2[18] = 30;
-  A2[3] = 5; A2[7] = 14; A2[11] = 21; A2[15] = 26; A2[19] = 28;
-  printf("before :\n");
-  affiche(m,n,A2,m,stdout);
-  //mylapack_dgetf2(LAPACK_COL_MAJOR, m, n, A2, m, ipiv2);
-  b_mylapack_dgetrf(LAPACK_COL_MAJOR, m, n, A2, m, ipiv2, bsize);
-  matrix_AtoLU(m, n, A2, m, L2, m, U2, min_mn);
+  printf("\tok\n");
   
-  printf("\n L :\n");
-  affiche(m,min_mn,L2,m,stdout);
-  printf("\n U :\n");
-  affiche(min_mn,n,U2,min_mn,stdout);
-  printf("ok\n");
-
-  free(A2);
-  free(L2);
-  free(U2);
-  free(ipiv2);*/
-
-
   free(A);
   free(L);
   free(U);
   free(C);
 }
 
-
+void test_dgetrf(){
+  int m = 100;
+  int n = 95;
+  int bsize = 30;
+  int min_mn = MIN(m,n);
+  double *A = matrix_rand(m,n);
+  double *C = matrix_alloc(m,n);
+  double *L = matrix_alloc(m,min_mn);
+  double *U = matrix_alloc(min_mn,n);
+  int ipiv[m];
+  int i;
+  memcpy(C, A, m*n*sizeof(double));
+  printf("Testing mylapack_dgetrf...");
+  b_mylapack_dgetrf(LAPACK_COL_MAJOR, m, n, A, m, ipiv, bsize);
+  matrix_AtoLU(m, n, A, m, L, m, U, min_mn);
+  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, min_mn, 1.0, L, m, U, min_mn, 0.0, A, m);
+  for(i = 0; i < m*n; ++i){
+    ASSERT_EQ(A[i], C[i]);
+  }
+  printf("\tok\n");
+  free(A);
+  free(C);
+  free(L);
+  free(U);
+}
 
 int main(){
   test_dget_();
+  test_dgetrf();
   return 0;
 }
